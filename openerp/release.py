753dc8af8a953773f590c42304f01d947989173d
GreenOpenERP <greenopenerp@gmail.com>
2014-04-01 08:18:21 +0800

    GreenOpenERP-7.0-20140401-win-3ee4c95-198d9e4-f822302
    https://github.com/GreenOpenERP/openobject-server-7.0/commit/3ee4c95
    https://github.com/GreenOpenERP/openerp-web-7.0/commit/198d9e4
    https://github.com/GreenOpenERP/openobject-addons-7.0/commit/f822302

--- openerp/release.py
+++ openerp/release.py
@@ -52,5 +52,5 @@ license = 'AGPL-3'
 
 nt_service_name = "openerp-server-" + serie
 
-version = "GreenOpenERP-7.0-20140401-win-1158f54-198d9e4-f822302"
+version = "GreenOpenERP-7.0-20140401-win-3ee4c95-198d9e4-f822302"
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
